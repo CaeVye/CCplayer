ccd27f99faa754b81d577241ec83aa2f8fd75fe7
CaeVye <caevye@posteo.de>
2017-05-29 18:44:19 +0200

    Missing download label added to activity

--- app/src/main/java/com/example/android/ccplayer/MainActivity.java
+++ app/src/main/java/com/example/android/ccplayer/MainActivity.java
@@ -13,13 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Functionality for button to add a comment
 
     public void openCommentEntry(View view) {
         Intent i = new Intent(this, AddComment.class);
         startActivity(i);
-        //write code here
     }
 
+    //Functionality to play the offline library
 
     public void openPlayLibrary(View view) {
         Intent i = new Intent(this, PlayLibrary.class);
@@ -27,17 +28,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //Functionality to play the online stream
+
     public void openPlayStream(View view) {
         Intent i = new Intent(this, PlayStream.class);
         startActivity(i);
     }
 
+    //Functionality to send a file by e-mail
 
     public void openSend(View view) {
         Intent i = new Intent(this, SendActivity.class);
         startActivity(i);
     }
 
+    //Functionality to download a streaming file to the offline library
 
     public void openDownload(View view) {
         Intent i = new Intent(this, Download.class);
